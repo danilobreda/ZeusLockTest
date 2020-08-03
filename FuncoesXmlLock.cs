f114bc3f76a0845e63380edd90fd61c2d4e14bbb
DaniloBreda <danilo@bredas.com.br>
2020-08-02 23:58:22 -0300

    Add project files.

---
+++ FuncoesXmlLock.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace TesteCacheZeus
+{
+    public static class FuncoesXmlLock
+    {
+        private static readonly Hashtable CacheSerializers = new Hashtable();
+
+        /// <summary>
+        ///     Serializa a classe passada para uma string no form
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objeto"></param>
+        /// <returns></returns>
+        public static string ClasseParaXmlString<T>(T objeto)
+        {
+            XElement xml;
+            var keyNomeClasseEmUso = typeof(T).FullName;
+            var ser = BuscarNoCache(keyNomeClasseEmUso, typeof(T));
+
+            using (var memory = new MemoryStream())
+            {
+                using (TextReader tr = new StreamReader(memory, Encoding.UTF8))
+                {
+                    ser.Serialize(memory, objeto);
+                    memory.Position = 0;
+                    xml = XElement.Load(tr);
+                    xml.Attributes().Where(x => x.Name.LocalName.Equals("xsd") || x.Name.LocalName.Equals("xsi")).Remove();
+                }
+            }
+            return XElement.Parse(xml.ToString()).ToString(SaveOptions.DisableFormatting);
+        }
+
+        /// <summary>
+        ///     Deserializa a classe a partir de uma String contendo a estrutura XML daquela classe
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static T XmlStringParaClasse<T>(string input) where T : class
+        {
+            var keyNomeClasseEmUso = typeof(T).FullName;
+            var ser = BuscarNoCache(keyNomeClasseEmUso, typeof(T));
+
+            using (var sr = new StringReader(input))
+                return (T)ser.Deserialize(sr);
+        }
+
+        private static XmlSerializer BuscarNoCache(string chave, Type type)
+        {
+            lock (type)
+            {
+                if (CacheSerializers.Contains(chave))
+                {
+                    return (XmlSerializer)CacheSerializers[chave];
+                }
+
+                var ser = XmlSerializer.FromTypes(new[] { type })[0];
+                CacheSerializers.Add(chave, ser);
+
+                return ser;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace TesteCacheZeus
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //var summary = BenchmarkRunner.Run<teste>();
+            string xml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "xml.xml"));
+
+            Console.WriteLine("Digite 1 para teste sem lock e 2 para teste com lock => ");
+
+            string saida = Console.ReadLine();
+            if (saida == "1")
+            {
+                Console.WriteLine("Iniciando processo SEM lock");
+                Thread threadA = new Thread(() => FuncoesXml.XmlStringParaClasse<NFe.Classes.NFe>(xml));
+                Thread threadB = new Thread(() => FuncoesXml.XmlStringParaClasse<NFe.Classes.NFe>(xml));
+                Thread threadC = new Thread(() => FuncoesXml.XmlStringParaClasse<NFe.Classes.NFe>(xml));
+
+                threadA.Start();
+                threadB.Start();
+                threadC.Start();
+            }
+            else if (saida == "2")
+            {
+                Console.WriteLine("Iniciando processo COM lock");
+                Thread threadA = new Thread(() => FuncoesXmlLock.XmlStringParaClasse<NFe.Classes.NFe>(xml));
+                Thread threadB = new Thread(() => FuncoesXmlLock.XmlStringParaClasse<NFe.Classes.NFe>(xml));
+                Thread threadC = new Thread(() => FuncoesXmlLock.XmlStringParaClasse<NFe.Classes.NFe>(xml));
+
+                threadA.Start();
+                threadB.Start();
+                threadC.Start();
+            }
+
+            Console.WriteLine("Processo Finalizado");
+        }
+    }
+}
